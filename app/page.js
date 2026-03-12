d6b7d117383755956095da3ca1268c554107c142
Azhar Khan <azharkhan844716@gmail.com>
2026-03-12 20:49:04 +0530

    feat:added required components

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      AZhar khan <br/>
+      <Button variant="link">Click</Button>
+    </div>
   );
 }
