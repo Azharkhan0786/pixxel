2fec34ca544e25fa252a4b42d497fd06f6397fc6
Azhar Khan <azharkhan844716@gmail.com>
2026-03-20 02:59:04 +0530

    feat:complete header component

--- components/header.jsx
+++ components/header.jsx
@@ -3,11 +3,20 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { SignInButton, SignUpButton, UserButton, Show } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, UserButton,  } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { useStoreUser } from "@/hooks/use-store-user";
+import { BarLoader } from "react-spinners";
+import { Unauthenticated,Authenticated } from "convex/react";
 
 const Header = () => {
   const path = usePathname();
+  const { isLoading } = useStoreUser();
+
+  if (path.includes("/editor")) {
+    return null; //Hide header on the editor page
+  }
+
   return (
     <header className="fixed top-6 transform  left-1/2 z-50 text-nowrap  -translate-x-1/2">
       <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-8 py-3 flex items-center justify-between gap-8 ">
@@ -45,7 +54,7 @@ const Header = () => {
         )}
 
         <div className="flex items-center gap-3 ml-10 md:ml20">
-          <Show when="signed-out">
+          <Unauthenticated>
             {/* Glass Sign In */}
             <SignInButton>
               <button className="backdrop-blur-md bg-white/10 border border-white/20 text-white rounded-xl font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
@@ -56,22 +65,31 @@ const Header = () => {
             {/* Primary Sign Up */}
             <SignUpButton>
               <button className="bg-[#6c47ff] text-white rounded-xl font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
+                GetStarted
               </button>
             </SignUpButton>
-          </Show>
+          </Unauthenticated>
 
-          <Show when="signed-in">
-            <UserButton 
-            appearance={{
-              elements:{
-                avatarBox:"w-8 h-8"
+          <Authenticated>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-8 h-8"
 
-              }
-            }} 
+                }
+              }}
             />
-          </Show>
+          </Authenticated>
         </div>
+
+        {isLoading && (
+          <div className="fixed bottom-0 left-0 w-full z-40 flex justify-center">
+            <BarLoader
+              width={"95%"}
+              color="#06b6d4"
+            />
+          </div>
+        )}
       </div>
     </header>
   );
