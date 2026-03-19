f6da98e33378f914ba7e035535a2ac0e996a52c0
Azhar Khan <azharkhan844716@gmail.com>
2026-03-19 14:12:48 +0530

    feat:added required components

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      AZhar khan <br/>
       <Button variant="link">Click</Button>
     </div>
   );
--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="fixed top-6 transform  left-1/2 z-50 text-nowrap  -translate-x-1/2">
+      <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-8 py-3 flex items-center justify-between gap-8 ">
+        Header
+      </div>
+      
+    </header>
+  );
+};
 
-export default Header
+export default Header;
